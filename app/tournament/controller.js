3a419b5726e86c626f8a8abe8fa1419db39a3cbf
DesperadoUa <Lazarev-konstant@mail.ru>
2023-11-03 17:14:56 +0200

    fix result

--- app/tournament/controller.js
+++ app/tournament/controller.js
@@ -2,17 +2,18 @@ const { Router } = require('express')
 const router = Router()
 const Service = require('./service')
 router.post('/tournament', async (req, res) => {
+	let result
 	if (req.body.action === 'visit') {
-		await Service.insertVisit({
+		result = await Service.insertVisit({
 			userID: req.body.id,
 			date: new Date().toISOString().replace('T', ' ').slice(0, 19)
 		})
 	} else if (req.body.action === 'click') {
-		await Service.insertAction({
+		result = await Service.insertAction({
 			userID: req.body.id,
 			date: new Date().toISOString().replace('T', ' ').slice(0, 19)
 		})
 	}
-	res.status(200).json({ status: 'ok' })
+	res.status(200).json({ status: result })
 })
 module.exports = router
