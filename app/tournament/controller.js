5689242d4475a2d1ef36216352ececb04dded887
DesperadoUa <Lazarev-konstant@mail.ru>
2023-11-06 10:21:31 +0200

    fix controller cod

--- app/tournament/controller.js
+++ app/tournament/controller.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
 const router = Router()
 const Service = require('./service')
-router.post('/tournament', async (req, res) => {
-	let result
-	if (req.body.action === 'visit') {
-		result = await Service.insertVisit({
-			userID: req.body.id,
-			date: new Date().toISOString().replace('T', ' ').slice(0, 19)
-		})
-	} else if (req.body.action === 'click') {
-		result = await Service.insertAction({
-			userID: req.body.id,
-			date: new Date().toISOString().replace('T', ' ').slice(0, 19)
-		})
-	}
+const DateTime = require('../../helpers/dateTime')
+router.post('/tournament/visit', async (req, res) => {
+	const result = await Service.insertVisit({
+		userID: req.body.id,
+		date: DateTime.currentDate()
+	})
+	res.status(200).json({ status: result })
+})
+router.post('/tournament/click', async (req, res) => {
+	const result = await Service.insertVisit({
+		userID: req.body.id,
+		date: DateTime.currentDate()
+	})
 	res.status(200).json({ status: result })
 })
 module.exports = router
